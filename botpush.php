2cfcbe641fdc0ac3c04901d953d292ac31715158
Kraisri <kraisri.n@gmail.com>
2018-10-07 11:45:13 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,12 +8,15 @@ $access_token = 'uB+caqQPQ4E0qTpMIebEgIU/LbqlqvKUna/RetLvgYz9eij00sCPPGtLxAV3yo7
 
 $channelSecret = 'bd2b748e66d2081807ad9281be4afe39';
 
+//กรณีส่งคนเดียว ให้แยกเป็น ID ******
 $pushID = 'U68140a2a00330006c76aee0172d323a9'; //Id น้องโบนัส
+$pushID = 'U88505162ce5a8b2d26a1a4567fa3ac8c'; //Id แม่แต้ม
+//กรณีส่งคนเดียว ให้แยกเป็น ID ******
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('สวัสดีน้องโบนัส จากพ่อหนึ่ง');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('สวัสดีแม่แต้ม จากพ่อหนึ่ง');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
